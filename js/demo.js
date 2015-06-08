ba8e22ccf03e5797d145cacfec6435454d96d6c3
José Gonçalves <goncalves.joseh@gmail.com>
2015-06-09 00:53:06 +0200

    Slide on scroll effect added

--- js/demo.js
+++ js/demo.js
@@ -24,10 +24,13 @@ WebFontConfig = {
 
 // Initialize Share-Buttons
 $.shareButtons({
-  facebook : { use: true, link: 'https://www.facebook.com/pages/mycompany', extras: 'target="_blank"' },
-  linkedin : { use: true, link: 'https://www.linkedin.com/company/mycompany' },
-  google :   { use: true, link: 'https://plus.google.com/myidongoogle' },
-  mybutton : { use: true, link: 'http://github.com', icon: 'github', class: 'git', title: 'My title for the button' },
-  phone :    { use: true, link: '+000' },
-  email :    { use: true, link: 'test@web.com' }
+  effect  : 'slide-on-scroll',
+  buttons : {
+    'facebook':   { class: 'facebook', use: true, link: 'https://www.facebook.com/pages/mycompany', extras: 'target="_blank"' },
+    'linkedin':   { class: 'linkedin', use: true, link: 'https://www.linkedin.com/company/mycompany' },
+    'google':     { class: 'gplus',    use: true, link: 'https://plus.google.com/myidongoogle' },
+    'mybutton':   { class: 'git',      use: true, link: 'http://github.com', icon: 'github', title: 'My title for the button' },
+    'phone':      { class: 'phone separated',    use: true, link: '+000' },
+    'email':      { class: 'email',    use: true, link: 'test@web.com' }
+  }
 });
--- js/jquery.contact-buttons.js
+++ js/jquery.contact-buttons.js
@@ -9,85 +9,114 @@
  */
  
 (function ($) {
-    'use strict';
+  'use strict';
 
-    $.shareButtons = function( options ){
-      
-      // Define the defaults
-      var defaults = { 
-        facebook : { class: 'facebook', use: false, icon: 'facebook',    link: '', title: 'Follow on Facebook' },
-        google :   { class: 'gplus',    use: false, icon: 'google-plus', link: '', title: 'Visit on Google Plus' },
-        linkedin : { class: 'linkedin', use: false, icon: 'linkedin',    link: '', title: 'Visit on LinkedIn' },
-        twitter :  { class: 'twitter',  use: false, icon: 'twitter',     link: '', title: 'Follow on Twitter' },
-        phone :    { class: 'phone',    use: false, icon: 'phone',       link: '', title: 'Call us', type: 'phone' },
-        email :    { class: 'email',    use: false, icon: 'envelope',    link: '', title: 'Send us an email', type: 'email' }
-      };
-
-      // Merge defaults and options, without modifying defaults
-      var s,
-          settings = options;
-      for (s in defaults) {
-        if (options[s]) {
-          settings[s] = $.extend( defaults[s], options[s] );
-        }
-      }
-      
-      // Define the container for the buttons
-      var oContainer = $("#contact-buttons-bar");
-
-      // Check if the container is already on the page
-      if ( oContainer.length === 0 ) {
-        
-        // Insert the container element
-        $('body').append('<div id="contact-buttons-bar">');
-        
-        // Get the just inserted element
-        oContainer = $("#contact-buttons-bar");
-        
-        // Add show/hide button
-        var sShowHideBtn = '<button class="contact-button-link show-hide-contact-bar"><span class="fa fa-angle-left"></span></button>';
-        oContainer.append(sShowHideBtn);
-        
-        var i;
-        for ( i in settings ) {
-          var bs = settings[i],
-              sLink = bs.link,
-              active = bs.use;
-          
-          // Check if element is active
-          if (bs.use === true) {
-            
-            // Change the link for phone and email when needed
-            if (bs.type === 'phone') {
-              sLink = 'tel:' + bs.link;
-            } else if (bs.type === 'email') {
-              sLink = 'mailto:' + bs.link;
-            }
-            
-            // Insert the links
-            var sIcon = '<span class="fa fa-' + bs.icon + '"></span>',
-                sButton = '<a href="' + sLink + 
-                            '"class="contact-button-link cb-ancor ' + bs.class + '" ' + 
-                            (bs.title ? 'title="' + bs.title + '"' : '') + 
-                            (bs.extras ? bs.extras : '') + 
-                            '>' + sIcon + '</a>';
-            oContainer.append(sButton);
-          }
-        }
-        
-        // Make the buttons visible
-        setTimeout(function(){
-          oContainer.animate({ left : 0 });
-        }, 200);
-        
-        // Show/hide buttons
-        $('body').on('click', '.show-hide-contact-bar', function(e){
-          e.preventDefault();
-          e.stopImmediatePropagation();
-          $('.show-hide-contact-bar').find('.fa').toggleClass('fa-angle-right fa-angle-left');
-          oContainer.find('.cb-ancor').toggleClass('cb-hidden');
-        });
+  // Main function
+  $.shareButtons = function( options ){
+    
+    // Define the defaults
+    var defaults = { 
+      effect  : '', // slide-on-scroll
+      buttons : {
+        'facebook':   { class: 'facebook',  use: false, icon: 'facebook',    link: '', title: 'Follow on Facebook' },
+        'google':     { class: 'gplus',     use: false, icon: 'google-plus', link: '', title: 'Visit on Google Plus' },
+        'linkedin':   { class: 'linkedin',  use: false, icon: 'linkedin',    link: '', title: 'Visit on LinkedIn' },
+        'twitter':    { class: 'twitter',   use: false, icon: 'twitter',     link: '', title: 'Follow on Twitter' },
+        'pinterest':  { class: 'pinterest', use: false, icon: 'pinterest',   link: '', title: 'Follow on Pinterest' },
+        'phone':      { class: 'phone',     use: false, icon: 'phone',       link: '', title: 'Call us', type: 'phone' },
+        'email':      { class: 'email',     use: false, icon: 'envelope',    link: '', title: 'Send us an email', type: 'email' }
       }
     };
+
+    // Merge defaults and options
+    var s,
+        settings = options;
+    for (s in defaults.buttons) {
+      if (options.buttons[s]) {
+        settings.buttons[s] = $.extend( defaults.buttons[s], options.buttons[s] );
+      }
+    }
+    
+    // Define the container for the buttons
+    var oContainer = $("#contact-buttons-bar");
+
+    // Check if the container is already on the page
+    if ( oContainer.length === 0 ) {
+      
+      // Insert the container element
+      $('body').append('<div id="contact-buttons-bar">');
+      
+      // Get the just inserted element
+      oContainer = $("#contact-buttons-bar");
+      
+      // Add class for effect
+      oContainer.addClass(settings.effect);
+      
+      // Add show/hide button
+      var sShowHideBtn = '<button class="contact-button-link show-hide-contact-bar"><span class="fa fa-angle-left"></span></button>';
+      oContainer.append(sShowHideBtn);
+      
+      var i;
+      for ( i in settings.buttons ) {
+        var bs = settings.buttons[i],
+            sLink = bs.link,
+            active = bs.use;
+        
+        // Check if element is active
+        if (bs.use === true) {
+          
+          // Change the link for phone and email when needed
+          if (bs.type === 'phone') {
+            sLink = 'tel:' + bs.link;
+          } else if (bs.type === 'email') {
+            sLink = 'mailto:' + bs.link;
+          }
+          
+          // Insert the links
+          var sIcon = '<span class="fa fa-' + bs.icon + '"></span>',
+              sButton = '<a href="' + sLink + 
+                          '"class="contact-button-link cb-ancor ' + bs.class + '" ' + 
+                          (bs.title ? 'title="' + bs.title + '"' : '') + 
+                          (bs.extras ? bs.extras : '') + 
+                          '>' + sIcon + '</a>';
+          oContainer.append(sButton);
+        }
+      }
+      
+      // Make the buttons visible
+      setTimeout(function(){
+        oContainer.animate({ left : 0 });
+      }, 200);
+      
+      // Show/hide buttons
+      $('body').on('click', '.show-hide-contact-bar', function(e){
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        $('.show-hide-contact-bar').find('.fa').toggleClass('fa-angle-right fa-angle-left');
+        oContainer.find('.cb-ancor').toggleClass('cb-hidden');
+      });
+    }
+  };
+  
+  // Slide on scroll effect
+  $(function(){
+    
+    // Define element to slide
+    var el = $("#contact-buttons-bar.slide-on-scroll");
+    
+    // Load top default
+    el.attr('data-top', el.css('top'));
+    
+    // Listen to scroll
+    $(window).scroll(function() {
+      clearTimeout( $.data( this, "scrollCheck" ) );
+      $.data( this, "scrollCheck", setTimeout(function() {
+        var nTop = $(window).scrollTop() + parseInt(el.attr('data-top'));
+        el.animate({
+          top : nTop
+        }, 500);
+      }, 250) );
+    });
+  });
   
  }( jQuery ));
